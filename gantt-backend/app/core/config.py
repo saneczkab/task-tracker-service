2f9aefe4ff23a99a54459263e979892da84abb0c
Максим Зайцев <144555287+maksimzaitsevV@users.noreply.github.com>
2025-11-23 17:03:18 +0500

    Implement docker with bugs

--- gantt-backend/app/core/config.py
+++ gantt-backend/app/core/config.py
@@ -7,8 +7,11 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_DAYS: int = 30
     DATABASE_URL: str
 
-    model_config = {"env_file": ".env",
-                    "env_prefix": "AUTH_"}
+    model_config = {
+        "env_file": "../.env",
+        "env_prefix": "AUTH_",
+        "extra": "allow"
+    }
 
 
 settings = Settings()
