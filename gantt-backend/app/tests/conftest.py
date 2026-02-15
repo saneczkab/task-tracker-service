252f7d66faa9d6e9b128acbae0248ddfaf325f93
Alexandr Iarmoshenko <a2398253@gmail.com>
2026-02-15 12:20:16 +0500

    Fix tests format

--- gantt-backend/app/tests/conftest.py
+++ gantt-backend/app/tests/conftest.py
@@ -6,6 +6,5 @@ from dotenv import load_dotenv
 
 @pytest.fixture(scope="session", autouse=True)
 def load_env_and_models():
-    env_path = Path(__file__).parent.parent.parent / '.env'
+    env_path = Path(__file__).parent.parent.parent / ".env"
     load_dotenv(env_path)
-
