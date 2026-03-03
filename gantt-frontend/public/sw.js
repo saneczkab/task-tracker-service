dcebfad25777d65157007523b59d713d33944455
Alexandr Iarmoshenko <a2398253@gmail.com>
2026-03-03 15:56:30 +0500

    Update push link

--- gantt-frontend/public/sw.js
+++ gantt-frontend/public/sw.js
@@ -3,12 +3,20 @@ self.addEventListener("push", (event) => {
 
   event.waitUntil(
     self.registration.showNotification(data.title, {
-      body: data.body || "",
+      body: data.body,
+      data: {
+        team_id: data.team_id,
+        stream_id: data.stream_id,
+      },
     }),
   );
 });
 
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
-  event.waitUntil(clients.openWindow("/"));
+  const teamId = event.notification.data.team_id;
+  const streamId = event.notification.data.stream_id;
+  const url = `/team/${teamId}/stream/${streamId}`;
+
+  event.waitUntil(clients.openWindow(url));
 });
